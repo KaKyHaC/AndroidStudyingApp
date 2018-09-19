98cc73346edc6dc7d38555d5fef8f4bfc101857f
deman2332 <dimas97@mail.ru>
2018-09-19 14:42:06 +0300

    logging lifecycle

--- app/src/main/java/com/example/dima/studyingapplication/MainActivity.java
+++ app/src/main/java/com/example/dima/studyingapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dima.studyingapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,12 +11,16 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
+    private static final String TAG = "MainActivity";
+
     EditText editText;
     Button button;
     TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
 
@@ -32,5 +37,35 @@ public class MainActivity extends Activity implements View.OnClickListener{
             String text = editText.getText().toString();
             textView.setText(text);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
