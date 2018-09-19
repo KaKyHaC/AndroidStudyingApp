9701efd16a2fe8afe56ee188847b3d99ac6f3879
deman2332 <dimas97@mail.ru>
2018-09-19 16:06:29 +0300

    SecondActivity: add recycle view adapter

--- app/src/main/java/com/example/dima/studyingapplication/SecondActivity.java
+++ app/src/main/java/com/example/dima/studyingapplication/SecondActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends Activity {
 
@@ -19,15 +26,69 @@ public class SecondActivity extends Activity {
         setContentView(recyclerView);
     }
 
+    private static List<String> getStringList() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add("text" + i);
+        }
+        return list;
+    }
+
     private static RecyclerView initRecycleView (Context context) {
         RecyclerView recyclerView = new RecyclerView(context);
-
-        recyclerView.setBackgroundColor(Color.CYAN);
 
         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         recyclerView.setLayoutParams(layoutParams);
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+        MyAdapter adapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setData(getStringList());
 
         return recyclerView;
+    }
+
+    private static class MyAdapter extends RecyclerView.Adapter {
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView textView;
+
+            MyViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView;
+                textView.setBackgroundColor(Color.YELLOW);
+                textView.setTextColor(Color.BLUE);
+            }
+        }
+
+        List<String> data = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = new TextView(parent.getContext());
+            ViewGroup.LayoutParams layoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            textView.setLayoutParams(layoutParam);
+
+            return new MyViewHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            ((MyViewHolder)holder).textView.setText(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        public void setData(List<String> data) {
+            this.data = data;
+            notifyDataSetChanged();
+        }
     }
 }
